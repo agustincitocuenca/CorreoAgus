0e4bedbafa1b630404b7d017b161c8f2727e1495
agustincitocuenca <142261330+agustincitocuenca@users.noreply.github.com>
2023-10-04 17:35:50 -0300

    a

---
+++ src/main/java/correo/Correo.java
@@ -0,0 +1,24 @@
+package correo;
+
+public class Correo {
+
+    public Correo(String correoElectronico, String destinatario, String asunto, String cuerpo) {
+    }
+
+    public String getRemitente() {
+        return null;
+    }
+
+    public String getAsunto() {
+        return null;
+    }
+
+    public String getCuerpo() {
+        return null;
+    }
+
+    public String getDestinatario() {
+        return null;
+    }
+    
+}
